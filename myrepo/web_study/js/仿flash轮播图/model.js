8b79223478a05375f94f09acff3a3f80555b5c02
lyucan <lyucan_1@163.com>
2018-09-15 21:57:24 +0800

    some change

---
+++ myrepo/web_study/js/仿flash轮播图/model.js
@@ -0,0 +1,39 @@
+function getStyle(obj, name) {
+    if(obj.currentStyle)
+    {
+        return obj.currentStyle[name];
+    }
+    else
+    {
+        return getComputedStyle(obj, null)[name];
+    }
+}
+
+function startMove(obj, attr, iTarget) {
+    clearInterval(obj.timer);
+    obj.timer = setInterval(function () {
+        var cur = 0;
+        if(attr == 'opacity'){
+            cur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
+        }
+        else
+        {
+            cur = parseInt(getStyle(obj, attr));
+        }
+        // alert(cur)
+        var speed = (iTarget - cur)/6;
+        speed = speed>0?Math.ceil(speed):Math.floor(speed);
+
+        if (cur == iTarget){
+            clearInterval(obj.timer)
+        }
+        else {
+            if(attr == 'opacity'){
+                obj.style[attr] = (cur + speed)/100;
+            }
+            else{
+                obj.style[attr] = cur + speed + 'px';
+            }
+
+        }
+    },30)
