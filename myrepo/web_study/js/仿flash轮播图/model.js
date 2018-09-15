a0f4e209307dd368b12902afc0ac170e59012b49
lyucan <lyucan_1@163.com>
2018-09-15 21:58:28 +0800

    some change

--- myrepo/web_study/js/仿flash轮播图/model.js
+++ myrepo/web_study/js/仿flash轮播图/model.js
@@ -13,27 +13,28 @@ function startMove(obj, attr, iTarget) {
     clearInterval(obj.timer);
     obj.timer = setInterval(function () {
         var cur = 0;
-        if(attr == 'opacity'){
+        if (attr == 'opacity') {
             cur = Math.round(parseFloat(getStyle(obj, attr)) * 100);
         }
-        else
-        {
+        else {
             cur = parseInt(getStyle(obj, attr));
         }
         // alert(cur)
-        var speed = (iTarget - cur)/6;
-        speed = speed>0?Math.ceil(speed):Math.floor(speed);
+        var speed = (iTarget - cur) / 6;
+        speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
 
-        if (cur == iTarget){
+        if (cur == iTarget) {
             clearInterval(obj.timer)
         }
         else {
-            if(attr == 'opacity'){
-                obj.style[attr] = (cur + speed)/100;
+            if (attr == 'opacity') {
+                obj.style[attr] = (cur + speed) / 100;
             }
-            else{
+            else {
                 obj.style[attr] = cur + speed + 'px';
             }
 
         }
-    },30)
+    }, 30)
+}
+
