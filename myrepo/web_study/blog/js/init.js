a3af73e52d389cb6eb7c5c3bc7ec6a44b7572994
lyucan_1 <lyucan_1@163.com>
2018-10-10 11:54:25 +0800

    some changes

--- myrepo/web_study/blog/js/init.js
+++ myrepo/web_study/blog/js/init.js
@@ -1,12 +1,12 @@
 //根据浏览器窗口的大小，来动态的设置主体内容的长度
 //1190px,990px
 function setWrap(){
-    if ($(window).width()>1190) {
+    if ($(window).width()>1260) {
         //1190px
-        $('body').attr('class','w1190');
+        $('body').attr('class','w1260');
     }else{
         //990px
-        $('body').attr('class','w990');
+        $('body').attr('class','w1190');
     }
 }
 setWrap();
