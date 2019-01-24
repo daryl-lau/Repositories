9062b79fa27f46baeb16a5bdfd96f4e2e6627135
jerry <lyucan_1@163.com>
2019-01-24 18:00:43 +0800

    some changes

--- myrepo/web_study/demos/jd放大镜/resource/js/main.js
+++ myrepo/web_study/demos/jd放大镜/resource/js/main.js
@@ -9,16 +9,30 @@ window.onload = function () {
 
     if (lis.length > 5){
         prevBtn.onclick = function () {
-
+            startMove(ul, {'left':0});
+            removeClass(this, 'hover');
+            addClass(this, 'disabled');
         };
 
         nextBtn.onclick = function () {
-            startMove(ul, {'left':228})
+            startMove(ul, {'left':-228});
+            addClass(this, 'disabled');
+            removeClass(this, 'hover')
         };
 
         prevBtn.onmouseover = function () {
-            if (!hasClass(this, 'disabled')){
-                addClass(this, 'hover');
+            if (parseInt(ul.offsetLeft) === 0){
+                addClass(this, 'disabled');
+            }else {
+                addClass(this, 'hover')
+            }
+        };
+
+        nextBtn.onmouseover = function () {
+            if (parseInt(ul.offsetLeft) === -228){
+                addClass(this, 'disabled');
+            }else {
+                addClass(this, 'hover')
             }
         };
 
@@ -26,11 +40,6 @@ window.onload = function () {
             removeClass(prevBtn, 'hover')
         };
 
-        nextBtn.onmouseover = function () {
-            if (!hasClass(this, 'disabled')){
-                addClass(this, 'hover');
-            }
-        };
         nextBtn.onmouseout = function(){
             removeClass(nextBtn, 'hover')
         };
