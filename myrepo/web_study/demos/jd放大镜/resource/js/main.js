bef8f1c1088bd43c13100efc2f31f7f57d8fd29c
jerry <lyucan_1@163.com>
2019-01-24 18:02:29 +0800

    some changes

--- myrepo/web_study/demos/jd放大镜/resource/js/main.js
+++ myrepo/web_study/demos/jd放大镜/resource/js/main.js
@@ -3,7 +3,6 @@ window.onload = function () {
 
     var prevBtn = document.querySelector('#prev');
     var nextBtn = document.querySelector('#next');
-
     var lis = document.querySelectorAll('.view_s_box li');
     var ul = document.querySelector('.view_s_box ul');
 
