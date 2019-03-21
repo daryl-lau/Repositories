4031f8d63a6d81c73204a0f721e85ec763bc1585
jerry <lyucan_1@163.com>
2019-03-21 16:14:55 +0800

    some changes

--- myrepo/web_study/M站/JDM站-全原生js/js/main.js
+++ myrepo/web_study/M站/JDM站-全原生js/js/main.js
@@ -1,18 +1,20 @@
 window.addEventListener('load', () => {
+
+    ////////////////////////轮播图开始////////////////////////////////////
+
     // 获取元素及一些参数
     // let ul = document.getElementsByTagName('ul')[0];
     // let ulLength = ul.getElementsByTagName('li').length;
     // let liWidth = ul.getElementsByTagName('li')[0].offsetWidth;
     // let ol = document.getElementsByTagName('ol')[0];
     // let pointWidth = 20;
-
+    // debugger;
     let ul = document.querySelector('#product .banner ul');
     let ulLength = ul.getElementsByTagName('li').length;
     // let liWidth = ul.getElementsByTagName('li')[0].offsetWidth;
     let liWidth = document.querySelector('#wrapper').offsetWidth;
     let ol = document.querySelector('#product .banner ol');
-    let pointWidth = 20;
-    console.log(liWidth);
+    let pointWidth = 12;
 
     // 拼接前后图片，最后一张加到开头，第一张加到结尾
     let firstImg = ul.firstElementChild.cloneNode(true);
@@ -22,18 +24,18 @@ window.addEventListener('load', () => {
 
     // 动态设置li宽度
     let allimgs = ul.querySelectorAll('img');
-    console.log(allimgs);
+    // console.log(allimgs);
     for (let i = 0; i < ulLength + 2; i++){
-        console.log(i);
+        // console.log(i);
         allimgs[i].style.width = liWidth + 'px';
     }
 
     // 动态设置ul的宽度
     ul.style.width = '' + liWidth * (ulLength + 2) + 'px';
-    console.log(ul.style.width);
+    // console.log(ul.style.width);
     // ul.style.transform: translateX(-520px);
     ul.style.transform = 'translateX(' + -liWidth + 'px)';
-    console.log(ul.style.transform);
+    // console.log(ul.style.transform);
 
     //动态添加指示器节点
     let str = '';
@@ -43,8 +45,11 @@ window.addEventListener('load', () => {
     ol.innerHTML = str;
     let lis = ol.getElementsByTagName('li');
     lis[0].className = 'current';
-    ol.style.width = '' + ulLength * pointWidth + 'px';
-    ol.style.marginLeft = '' + -ulLength * pointWidth / 2 + 'px';
+    ol.style.width = ulLength * pointWidth + 'px';
+    ol.style.marginLeft = -ulLength * pointWidth / 2 + 'px';
+
+
+
     // css3动画函数
     let addTransition = (obj) => {
         obj.style.transaction = 'all .2s ease';
@@ -128,5 +133,18 @@ window.addEventListener('load', () => {
         startX = 0;
         endX = 0;
         distanceX = 0;
+    });
+
+////////////////////////轮播图结束////////////////////////////////////
+
+
+    let btnClose = document.querySelector('.middle-img .icon-close');
+    let closeDiv = document.querySelector('.middle-img');
+    btnClose.addEventListener('click', ()=>{
+        debugger;
+        console.log(111);
+        closeDiv.style.display = 'none';
+        console.log(closeDiv);
     })
+
 });
