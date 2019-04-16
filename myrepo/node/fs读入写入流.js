3cc9c91712a3ffdad31f2d490e25572a0688153a
jerry <lyucan_1@163.com>
2019-04-16 20:45:27 +0800

    some changes

---
+++ myrepo/node/fs读入写入流.js
@@ -0,0 +1,17 @@
+let fs = require('fs');
+
+let rs = fs.createReadStream('./v.f30.mp4');
+let ws = fs.createWriteStream('./new.mp4');
+
+rs.on('data', (data)=>{
+    console.log(data);
+    ws.write(data);
+});
+
+
+
+
+rs.once('open', ()=>{console.log('读入通道已开启')});
+rs.once('close', ()=>{console.log('读入通道已关闭')});
+ws.once('open', ()=>{console.log('写入通道已开启')});
+ws.once('close', ()=>{console.log('写入通道已关闭')});
