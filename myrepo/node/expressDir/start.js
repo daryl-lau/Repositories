cd6170850c8571addba0050632a5ccca3b14e369
jerry <lyucan_1@163.com>
2019-04-19 00:07:49 +0800

    some changes

--- myrepo/node/expressDir/start.js
+++ myrepo/node/expressDir/start.js
@@ -2,8 +2,34 @@ const express = require('express');
 
 const app = express();
 
+
+// 中间件，可以单独匹配某个uri
+app.use((req, res, next)=>{
+    console.log(111);
+    next();
+});
+
+app.use('/cc',(req, res, next)=>{
+    console.log('cccc');
+    next();
+});
+
+
+
 app.get('/',(req, res)=>{
-    res.write('hello!');
+    res.write('hello!1111');
+    res.end();
+});
+
+
+// 中间件如果没有res.end()，而是使用的next()，则会继续执行下面匹配到的代码
+// app.get('/',(req, res, next)=>{
+//     res.write('hello!1111');
+//     next();
+// });
+
+app.get('/', (req, res)=>{
+    res.write('hello!2222');
     res.end();
 });
 
@@ -11,5 +37,8 @@ app.get('/app',(req, res)=>{
     res.write('hello,app!');
     res.end();
 });
+
+
+
 
 app.listen(3000);
