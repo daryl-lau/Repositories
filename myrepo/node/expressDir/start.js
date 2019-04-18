3c9139ac913a6b4d66d83f4382bbf7ff6ca8ed16
jerry <lyucan_1@163.com>
2019-04-18 23:33:21 +0800

    some changes

--- myrepo/node/expressDir/start.js
+++ myrepo/node/expressDir/start.js
@@ -2,12 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
+app.get('/',(req, res)=>{
     res.write('hello!');
     res.end();
 });
-app.post();
 
-app.listen(3000, ()=>{
-
-});
+app.listen(3000);
