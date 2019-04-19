fe215dd4dd835754bb4e23a57e561de1cd8cbc17
jerry <lyucan_1@163.com>
2019-04-19 21:35:17 +0800

    some changes

--- myrepo/node/project/p1/app.js
+++ myrepo/node/project/p1/app.js
@@ -1,9 +1,11 @@
+// 引入包
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// 引入路由文件
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
