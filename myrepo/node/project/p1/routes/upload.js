41eb4daaa17223be06d4a55a8fe1c7536424571a
jerry <lyucan_1@163.com>
2019-04-23 00:47:33 +0800

    some changes

--- myrepo/node/project/p1/routes/upload.js
+++ myrepo/node/project/p1/routes/upload.js
@@ -3,18 +3,22 @@ var router = express.Router();
 var formidable = require('formidable');
 
 /* GET home page. */
-router.post('/', function (req, res, next) {
+router.post('/', (req, res, next) => {
     const form = new formidable.IncomingForm();
     form.uploadDir = '../UploadDir';
     form.keepExtensions = true;
     form.parse(req, function (err, fields, files) {
-        if(err){
+        if (err) {
             throw err;
         }
-        // console.log(fields);
+        console.log(fields);
         console.log(1);
-        res.end('1');
+        res.end('success');
     });
+    // form.on('progress', function (bytesReceived, bytesExpected) {
+    //     console.log(bytesExpected);
+    //     console.log(bytesReceived);
+    // });
 });
 
 module.exports = router;
