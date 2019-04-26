18b8ad5b378191c67c99a4cd3f6d57330b5337f5
jerry <lyucan_1@163.com>
2019-04-26 20:18:19 +0800

    some changes

--- myrepo/node/promise/promise.js
+++ myrepo/node/promise/promise.js
@@ -1,6 +1,36 @@
 const fs = require('fs');
 
 // 1、创建promise对象，一经创建，立马执行
-new Promise((resolve, reject)=>{
-    fs.readFile()
-})
+new Promise((resolve, reject) => {
+    fs.readFile(__dirname + '/data/a.txt', (err, data) => {
+        if (err) {
+            reject(err);
+        } else {
+            resolve(data);
+        }
+    });
+}).then((data) => {
+    console.log(data.toString());
+    return new Promise((resolve, reject) => {
+        fs.readFile(__dirname + '/data/b.txt', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    })
+}).then((data) => {
+    console.log(data.toString());
+    return new Promise((resolve, reject) => {
+        fs.readFile(__dirname + '/data/c.txt', (err, data) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        });
+    })
+}).then((data)=>{
+    console.log(data.toString());
+});
