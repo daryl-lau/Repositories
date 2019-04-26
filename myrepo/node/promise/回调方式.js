f1ca121ebfc8322c58e565588cae140d773baed6
jerry <lyucan_1@163.com>
2019-04-26 20:05:45 +0800

    some changes

--- myrepo/node/promise/回调方式.js
+++ myrepo/node/promise/回调方式.js
@@ -1,9 +1,23 @@
 const fs = require('fs');
 
-fs.readFile(__dirname + '/data/a.txt', (err, data)=>{
-    if(err){
+fs.readFile(__dirname + '/data/a.txt', (err, data) => {
+    if (err) {
         throw err;
-    }else {
+    } else {
         console.log(data.toString());
+        fs.readFile(__dirname + '/data/b.txt', (err, data) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log(data.toString());
+                fs.readFile(__dirname + '/data/c.txt', (err, data) => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log(data.toString());
+                    }
+                });
+            }
+        });
     }
 });
