59d9e6f21e87d6f8c9cbe17d39fb801fb9ad1bf7
jerry <lyucan_1@163.com>
2019-05-16 17:50:11 +0800

    some changes

--- myrepo/web/框架/Vue/VueStudy/vue-client/vue_demo/src/components/Footer.vue
+++ myrepo/web/框架/Vue/VueStudy/vue-client/vue_demo/src/components/Footer.vue
@@ -4,7 +4,7 @@
       <input type="checkbox"/>
     </label>
     <span>
-          <span>已完成0件</span> / 总计{{todos.length}}件
+          <span>已完成{{finishedCount}}件</span> / 总计{{todos.length}}件
         </span>
     <button class="btn btn-warning">清除已完成任务</button>
   </div>
@@ -14,7 +14,38 @@
   export default {
     name: "Footer",
     props: {
-      todos:Array
+      todos:Array,
+    },
+    methods:{
+      // 使用函数进行统计已完成的任务，调用时需要加()
+      // countFinished(){
+      //   let count = 0;
+      //   for (var i=0; i<this.todos.length; i++){
+      //     if (this.todos[i].finished){
+      //       count += 1
+      //     } else {
+      //       count += 0
+      //     }
+      //   }
+      //   return count
+      // }
+    },
+    computed: {
+      // 使用计算属性进行统计已完成的任务，调用时不需要加()
+       finishedCount(){
+          /*const count = (total, todo)=>{
+              return total + (todo.finished ? 1 : 0)
+          };*/
+          return this.todos.reduce((total, todo)=> total + (todo.finished ? 1 : 0), 0);
+       },
+       // isCheck: {
+       //    get(){
+       //       return this.finishedCount === this.todos.length && this.todos.length > 0;
+       //    },
+       //    set(value){
+       //       this.isSelectedAll(value)
+       //    }
+       // }
     }
   }
 </script>
