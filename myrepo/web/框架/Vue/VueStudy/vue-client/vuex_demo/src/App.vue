39884859c932e2fe7efb02a0fecbd8ec9da8d545
jerry <lyucan_1@163.com>
2019-05-18 00:34:03 +0800

    Enter

--- myrepo/web/框架/Vue/VueStudy/vue-client/vuex_demo/src/App.vue
+++ myrepo/web/框架/Vue/VueStudy/vue-client/vuex_demo/src/App.vue
@@ -1,112 +1,103 @@
 <template>
   <div class="todo-container">
     <div class="todo-wrap">
-      <Header 
-      :addTodo="addTodo"
-      ></Header>
-      <List
-        :todos="todos"
-        :delItem="delItem"
-      ></List>
-      <Footer
-        :todos="todos"
-        :delFinished="delFinished"
-        :isSelectedAll="isSelectedAll"
-      ></Footer>
+      <Header :addTodo="addTodo"></Header>
+      <List :todos="todos" :delItem="delItem"></List>
+      <Footer :todos="todos" :delFinished="delFinished" :isSelectedAll="isSelectedAll"></Footer>
     </div>
   </div>
 </template>
 
 <script>
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import List from "./components/List";
 
-  import Footer from './components/Footer';
-  import Header from './components/Header';
-  import List from './components/List'
-
-  export default {
-    name: 'App1',
-    components: {
-      Footer,
-      Header,
-      List
+export default {
+  name: "App1",
+  components: {
+    Footer,
+    Header,
+    List
+  },
+  data() {
+    return {
+      todos: [
+        { title: "学习Linux", finished: true },
+        { title: "学习Oracle", finished: false },
+        { title: "学习Oracle", finished: false },
+        { title: "学习Oracle", finished: false },
+        { title: "学习Python", finished: true },
+        { title: "学习Vue", finished: false },
+        { title: "学习JavaScript", finished: false }
+      ]
+    };
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.unshift(todo);
     },
-    data() {
-      return {
-        todos: [
-          {title: '学习Linux', finished: true},
-          {title: '学习Oracle', finished: false},
-          {title: '学习Oracle', finished: false},
-          {title: '学习Oracle', finished: false},
-          {title: '学习Python', finished: true},
-          {title: '学习Vue', finished: false},
-          {title: '学习JavaScript', finished: false}
-        ]
-      }
+    delItem(index) {
+      this.todos.splice(index, 1);
     },
-    methods: {
-      addTodo(todo) {
-        this.todos.unshift(todo);
-      },
-      delItem(index) {
-        this.todos.splice(index, 1)
-      },
-      delFinished() {
-        // filter 不会改变原始数组，因此如果想要更改数组，需要将结果重新赋值
-        this.todos = this.todos.filter((todo) => {
-          return todo.finished === false
-        })
-      },
-      isSelectedAll(isCheck) {
-        this.todos.forEach((todo) => {
-          todo.finished = isCheck;
-        })
-      },
+    delFinished() {
+      // filter 不会改变原始数组，因此如果想要更改数组，需要将结果重新赋值
+      this.todos = this.todos.filter(todo => {
+        return todo.finished === false;
+      });
+    },
+    isSelectedAll(isCheck) {
+      this.todos.forEach(todo => {
+        todo.finished = isCheck;
+      });
     }
   }
+};
 </script>
 
 <style>
-  /*app*/
-  body {
-    background: #fff;
-  }
+/*app*/
+body {
+  background: #fff;
+}
 
-  .btn {
-    display: inline-block;
-    padding: 8px 10px;
-    margin-bottom: 0;
-    font-size: 14px;
-    line-height: 20px;
-    text-align: center;
-    vertical-align: middle;
-    cursor: pointer;
-    box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
-    border-radius: 4px;
-  }
+.btn {
+  display: inline-block;
+  padding: 8px 10px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2),
+    0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
 
-  .btn-warning {
-    color: #fff;
-    background-color: orange;
-    border: none;
-  }
+.btn-warning {
+  color: #fff;
+  background-color: orange;
+  border: none;
+}
 
-  .btn-warning:hover {
-    color: #fff;
-    background-color: red;
-  }
+.btn-warning:hover {
+  color: #fff;
+  background-color: red;
+}
 
-  .btn:focus {
-    outline: none;
-  }
+.btn:focus {
+  outline: none;
+}
 
-  .todo-container {
-    width: 600px;
-    margin: 0 auto;
-  }
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
 
-  .todo-container .todo-wrap {
-    padding: 10px;
-    border: 1px solid #ddd;
-    border-radius: 5px;
-  }
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
