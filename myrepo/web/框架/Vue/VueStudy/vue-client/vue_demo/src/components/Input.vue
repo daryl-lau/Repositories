8efa6e6e2e79299e29fba40141b92b844ab7539e
jerry <lyucan_1@163.com>
2019-05-20 19:46:24 +0800

    Enter

--- myrepo/web/框架/Vue/VueStudy/vue-client/vue_demo/src/components/Input.vue
+++ myrepo/web/框架/Vue/VueStudy/vue-client/vue_demo/src/components/Input.vue
@@ -1,41 +1,40 @@
 <template>
   <div class="todo-header">
     <input
-      type="text" placeholder="请输入今天的任务清单，按回车键确认"
+      type="text"
+      placeholder="请输入今天的任务清单，按回车键确认"
       v-model.trim="inputMsg"
       @keyup.enter="addItem(inputMsg)"
-
-
-    />
+    >
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Input",
-    props: {addTodo: Function},
+export default {
+    name: 'Input',
+    props: { addTodo: Function },
     data() {
-      return {
-        inputMsg: '',
-      }
+        return {
+            inputMsg: ''
+        }
     },
     methods: {
-      addItem(msg) {
-        if (msg === '') {
-          alert('请输入内容！');
-          return
+        addItem(msg) {
+            if (msg === '') {
+                alert('请输入内容！')
+                return
+            }
+            const todo = { title: msg, finished: false }
+            this.addTodo(todo)
+            this.inputMsg = ''
         }
-        const todo = {title: msg, finished: false};
-        this.addTodo(todo);
-        this.inputMsg = '';
-      }
     }
-  }
+}
 </script>
 
 <style scoped>
-  /*header*/
-  .todo-header input {
+/*header*/
+.todo-header input {
     width: 560px;
     height: 28px;
     font-size: 14px;
@@ -43,11 +42,12 @@
     border-radius: 4px;
     padding: 4px 7px;
     outline: none;
-  }
+}
 
-  .todo-header input:focus {
+.todo-header input:focus {
     outline: none;
     border-color: rgba(255, 0, 0, 0.8);
-    box-shadow: inset 0 1px 1px rgba(255, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6);
-  }
+    box-shadow: inset 0 1px 1px rgba(255, 0, 0, 0.075),
+        0 0 8px rgba(255, 0, 0, 0.6);
+}
 </style>
