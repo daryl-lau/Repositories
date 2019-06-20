57ec558f55c4d8ade203ea79b2d6b5fda55d13d5
lyucan <lyucan_1@163.com>
2019-06-21 01:44:44 +0800

    some change

--- myrepo/nodejs/url/url.js
+++ myrepo/nodejs/url/url.js
@@ -1,18 +1,31 @@
 let http = require('http');
 let url = require('url');
 
-http.createServer((req, res)=>{
+http.createServer((req, res) => {
 
     console.log(req.url);   //   /aaa?username=admin&password=123123
 
-    console.log(url.parse(req.url, true));
+    // url模块parse方法可以传递参数true，直接将请求的数据转化为对象
+    let result = url.parse(req.url, true);
+    console.log(result);
+    /*
+    Url {
+        protocol: null,
+        slashes: null,
+        auth: null,
+        host: null,
+        port: null,
+        hostname: null,
+        hash: null,
+        search: '?username=admin&password=123123',
+        query: [Object: null prototype] { username: 'admin', password: '123123' },
+        pathname: '/aaa',
+        path: '/aaa?username=admin&password=123123',
+        href: '/aaa?username=admin&password=123123'
+    }
+*/
 
-
-    // querystring的parse方法可以将请求的数据转化为对象，通过&符分割
-    // let result = querystring.parse(query);
-    // console.log(result);  //   { username: 'admin', password: '123123' }
-    //
-    // console.log(result.username);   // admin
-    // console.log(result.password);   // 123123
+    let {pathname, query} = result;
+    console.log(pathname, query);  //  /aaa  [Object: null prototype] { username: 'admin', password: '123123' }
 
 }).listen(3000);
