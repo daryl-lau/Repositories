ce220abbbaaf18ab9773b63448d8945a6732f3b7
jerry <lyucan_1@163.com>
2019-06-21 23:40:07 +0800

    some changes

--- myrepo/nodejs/url/url.js
+++ myrepo/nodejs/url/url.js
@@ -5,7 +5,7 @@ http.createServer((req, res)=>{
 
     console.log(req.url);   //   /aaa?username=admin&password=123123
 
-    console.log(url.parse(req.url, true));
+    console.log(JSON.stringify(url.parse(req.url, true).query));
 
 
     // querystring的parse方法可以将请求的数据转化为对象，通过&符分割
