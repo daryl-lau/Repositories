a7e0d9a57442ed9f6cdda5b90e5c3c6f1abc572d
jerry <lyucan_1@163.com>
2019-06-22 00:59:00 +0800

    some changes

---
+++ myrepo/nodejs/ajax/FormData/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const multiparty = require('multiparty');
+
+http.createServer((req, res)=>{
+    let form = new multiparty.Form({
+        uploadDir: './uploads/'
+    });
+
+    form.parse(req);
+
+    form.on('filed', (name, value)=>{
+        console.log('filed:', name, value)
+    });
+
+    form.on('file', (name, file)=>{
+        console.log('file:', name, file)
+    });
+
+    form.on('close', ()=>{
+        console.log('上传成功')
+    });
+
+}).listen(8080);
