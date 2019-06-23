1425279dd34b087fcc060b97e0209286e2fe3b9d
jerry <lyucan_1@163.com>
2019-06-23 14:52:02 +0800

    some changes

--- myrepo/nodejs/webSocket/server.js
+++ myrepo/nodejs/webSocket/server.js
@@ -12,6 +12,7 @@ let ws = socket.listen(server);
 // 当ws建立连接的时候，会接受一个sock对象，在sock对象中有两个方法，emit和on， emit负责发送数据，on负责接收数据
 ws.on('connection', sock => {
     // 发送数据  sock.emit('name', data)， 发送的数据保持其原有的数据类型，不会发生改变，客户端发送过来的也是一样
+    // 发送和接收的名字需要客户端与服务端保持一致，否则无法接收
     // sock.emit();
     setInterval(function () {
         sock.emit('time', new Date().getTime());
