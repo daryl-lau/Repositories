4353700d31c6c483345d2d4c6d6beb3ce3e9abb4
jerry <lyucan_1@163.com>
2019-06-25 11:27:37 +0800

    some changes

--- myrepo/nodejs/MySQL/mysql异步.js
+++ myrepo/nodejs/MySQL/mysql异步.js
@@ -3,6 +3,8 @@ const mysql = require('mysql');
 const co = require('co-mysql');
 
 let connect = mysql.createPool({host: 'localhost', port: 3306, user: 'root', password: '', database: 'test'});
+
+// 用co-mysql将数据库连接包起来，就可以用async将异步转为同步了
 let db = co(connect);
 
 http.createServer(async (req, res) => {
