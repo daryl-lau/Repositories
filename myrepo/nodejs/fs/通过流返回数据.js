c26796b8e81868b0875aaea150e2c1d63bcfa1e0
jerry <lyucan_1@163.com>
2019-06-26 10:34:45 +0800

    some changes

--- myrepo/nodejs/fs/通过流返回数据.js
+++ myrepo/nodejs/fs/通过流返回数据.js
@@ -15,14 +15,11 @@ http.createServer((req, res) => {
         res.end()
     });
 
-    /*
-    response.setHeader(name, value) / response.setHeader(name) / response.removeHeader(name)
-    设置头部,获取和删除头部信息的，这个不同于 response.writeHead方法，这两个方法并没有真的把头部信息发送出去，发送头部信息只能通过调用
-    response.writeHead或response.write才会发送出去。
-    */
-
     // 设置响应头，在浏览器network 的Response Header中可以看到设置的响应头
+    // 如果不设置该响应头，则通过gzip压缩后，浏览器请求的时候会将请求的内容通过下载的方式下载下来，
+    // 因为不设置这个，浏览器不认识gzip格式的文件
     res.setHeader('content-encoding', 'gzip');
+
     let gz = zlib.createGzip();
     rs.pipe(gz).pipe(res)
 }).listen(3000);
---
+++ myrepo/nodejs/http/setHeader.js
@@ -0,0 +1,59 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const zlib = require('zlib');
+
+http.createServer((req, res) => {
+    let {pathname} = url.parse(req.url, true);
+
+    let rs = fs.createReadStream('.' + pathname);
+
+    // 如果不写错误处理函数，那么fs在找不到文件的时候，会直接抛出异常，导致程序中断
+    rs.on('error', () => {
+        res.writeHead(404);
+        res.write('404 not found');
+        res.end()
+    });
+
+    /*
+    response.setHeader(name, value) / response.setHeader(name) / response.removeHeader(name)
+    设置头部,获取和删除头部信息的，这个不同于 response.writeHead方法，这两个方法并没有真的把头部信息发送出去，发送头部信息只能通过调用
+    response.writeHead或response.write才会发送出去。
+    */
+
+    // 设置响应头，在浏览器network 的Response Header中可以看到设置的响应头
+    res.setHeader('content-encoding', 'gzip');
+
+    // 前台页面每1秒刷新一次
+    // res.setHeader('refresh', '1');
+
+    // 二秒跳到其它页面 response.setHeader("refresh","2;URL=otherPagename");
+    // 这里跳转只能本域跳转,后面的URL是拼接在域名后面的，而不是直接外部跳转
+    // res.setHeader("refresh","2;URL=login");
+
+    // 没有缓存：
+    // res.setHeader("Pragma", "No-cache");
+    // res.setHeader("Cache-Control", "no-cache");
+
+    //设置过期的时间期限
+    // res.setDateHeader("Expires", System.currentTimeMillis()+自己设置的时间期限);
+
+    // 訪问别的页面
+    // res.setStatus（302）;
+    // res.setHeader("location","url");
+
+    // 通知浏览器数据採用的压缩格式
+    // res.setHeader("Content-Encoding","压缩后的数据格式");
+
+    // 快速浏览器压缩数据的长度
+    // res.setHeader("Content-Length",压缩后的数据.length+"");
+
+    // 快速浏览器图片或视频
+    // res.setHeader("Content-type","这个參数在tomcat里conf下的web.xml里面找");
+
+    // 快速浏览器已下载的形式
+    // res.setHeader("Content-disposition","attachment;filename=2.jpg");
+
+    let gz = zlib.createGzip();
+    rs.pipe(gz).pipe(res)
+}).listen(3000);
