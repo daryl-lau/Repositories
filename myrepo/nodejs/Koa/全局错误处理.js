736aa501e80fc2b1d3269ca6a42785723e567617
jerry <lyucan_1@163.com>
2019-06-29 14:06:07 +0800

    some changes

--- myrepo/nodejs/Koa/全局错误处理.js
+++ myrepo/nodejs/Koa/全局错误处理.js
@@ -5,18 +5,32 @@ const Router = require('koa-router');
 let server = new Koa();
 server.listen(8080);
 
-server.use(async (ctx, next)=>{
-    try{
+server.use(async (ctx, next) => {
+    try {
         await next()
-    } catch(e) {
+    } catch (e) {
         ctx.throw(500, e)
     }
 });
 
-
 let router = new Router();
 
-router.get('/', ctx=>{
+
+/*
+
+同样可以在路由中进行错误捕获
+router.all('*', async ctx => {
+    try {
+        await next()
+    } catch (e) {
+        ctx.throw(500, e)
+    }
+});
+
+*/
+
+
+router.get('/', async ctx => {
 
     // 这里的错误会在上面捕获到
     ctx.body = dada.dasd
