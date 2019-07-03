1aae0442d9e8d29328a4bade4c15168f73b54d81
jerry <lyucan_1@163.com>
2019-07-03 16:59:23 +0800

    some changes

--- myrepo/nodejs/Koa_Project/routers/admin/index.js
+++ myrepo/nodejs/Koa_Project/routers/admin/index.js
@@ -1,9 +1,19 @@
 const Router = require('koa-router');
 const path = require('path');
+
+// 使用同步的文件读取模块
+const fs = require('await-fs');
+
 let router = new Router();
 
 router.get('/login', async ctx=>{
     await ctx.render('login', {})
+});
+
+router.post('/login', async ctx=>{
+    let file = await fs.readFile(path.resolve(__dirname, '../../admins.json'));
+    // console.log(JSON.parse(file.toString()));
+    ctx.body = JSON.parse(file.toString())
 });
 
 module.exports = router.routes();
