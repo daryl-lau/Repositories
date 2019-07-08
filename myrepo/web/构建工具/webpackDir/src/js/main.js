d3aa1a0cb6247bd4e43437a02be82caf28173068
jerry <lyucan_1@163.com>
2019-07-09 00:28:06 +0800

    some changes

---
+++ myrepo/web/构建工具/webpackDir/src/js/main.js
@@ -0,0 +1,12 @@
+import '../css/main.css'
+
+
+let a = 10;
+let b = 10;
+
+const sum = (n1, n2)=>{
+    return n1 + n2
+};
+
+
+alert(sum(a, b));
---
+++ myrepo/web/构建工具/webpackDir/webpack.config-file-loader.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+    // 模式 'none'  'production'  'development'
+    mode: 'production',
+    // 单入口文件配置
+    entry: './src/js/main.js',
+    // 出口文件
+    output: {
+        filename: 'bundle.js',
+        // 设置全路径
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    /*
+    多入口文件配置
+    entry: {
+        index_a: './src/js/1.js',
+        index_b: './src/js/2.js'
+    }
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+
+        // 这里[name]会替换成入口里面的键的值index_a index_b
+        filename: '[name].min.js'
+    }
+    */
+    module: {
+        // 规则数组，里面的每一个对象都是描述一个loader
+        rules: [
+            {
+                test: /\.css$/,
+                // use里面调用顺序是从后往前开始调用的
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            outputPath: './image'
+                        }
+                        // query: {
+                        //     name: 'img/[name]-[hash:5].[ext]'
+                        // }
+                    }
+                ]
+            }
+        ]
+    }
+};
