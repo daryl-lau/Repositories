4d258e674e54dec64f309d7965f87c79aee23ea4
jerry <lyucan_1@163.com>
2019-07-26 14:04:28 +0800

    some changes

--- myrepo/web/框架/React/react01/src/components/CommentList.js
+++ myrepo/web/框架/React/react01/src/components/CommentList.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, {Component, PureComponent} from 'react';
 import {Button} from "antd";
 
 // 展示组件, 即使数据相同，也会进行渲染，并且进行diff算法检测，会消耗服务器资源，可以使用shouldComponentUpdate生命周期来进行控制是否
 // 渲染，但是一旦用到生命周期，就需要使用class来定义组件
 /*
-function CommentList(props) {
+function Comment(props) {
     console.log('render');
     return (
         <div>
@@ -17,7 +17,7 @@ function CommentList(props) {
 */
 
 
-class CommentList extends Component {
+/*class CommentList extends Component {
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         return !(nextProps.data.content === this.props.data.content && nextProps.data.author === nextProps.data.author);
     }
@@ -30,10 +30,36 @@ class CommentList extends Component {
             </div>
         )
     }
+}*/
+
+
+/*
+* PureComponent 继承自 Component
+* 除了为你提供了一个具有浅比较的shouldComponentUpdate方法，PureComponent和Component基本上完全相同。
+*
+* 当props或者state改变时，PureComponent将对props和state进行浅比较。浅比较数组和对象时，仅比较引用是否相同，比较值时则比较值是否相等。
+* 所以在对数组或对象进行改变的时候，如果没有引用一个新的数组或对象，即使改变了数组或对象的内容，组件也不会重新渲染
+*
+* 但是Component不会比较当前和下个状态的props和state，因此，每当shouldComponentUpdate被调用时，组件默认的会重新渲染。
+* */
+
+class Comment extends PureComponent {
+    /*shouldComponentUpdate(nextProps, nextState, nextContext) {
+        return !(nextProps.data.content === this.props.data.content && nextProps.data.author === nextProps.data.author);
+    }*/
+
+    render() {
+        console.log('render');
+        return (
+            <div>
+                {this.props.content} ---{this.props.author}
+            </div>
+        )
+    }
 }
 
 
-class Comment extends Component {
+class CommentList extends Component {
     constructor(props) {
         super(props);
 
@@ -62,6 +88,7 @@ class Comment extends Component {
     render() {
 
         // 这样写会导致整个评论组件的重新渲染，不合理，应该是按照每一条评论进行渲染
+        // 对应上面Comment组件的function的写法
         // <CommentList data={this.state.data}/>
 
         //
@@ -69,10 +96,15 @@ class Comment extends Component {
         return (
             <div>
                 <Button type='primary' onClick={this.change}>修改</Button>
-                {this.state.data.map((i, index) => <CommentList data={i} key={index}/>)}
+
+                {/*对应上面Comment组件的Component的写法*/}
+                {/*{this.state.data.map((i, index) => <Comment data={i} key={index}/>)}*/}
+
+                {/*对应上面Comment组件的PureComponent的写法*/}
+                {this.state.data.map((i, index) => <Comment content={i.content} author={i.author}  key={index}/>)}
             </div>
         )
     }
 }
 
-export default Comment;
+export default CommentList;
