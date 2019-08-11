a84cb5a0dff5366003285027ea9c30472b2f6c8f
jerry <lyucan_1@163.com>
2019-08-11 10:52:52 +0800

    some changes

--- myrepo/web/框架/React/react01/src/components/RouterTest.js
+++ myrepo/web/框架/React/react01/src/components/RouterTest.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Link, Route} from 'react-router-dom'
+import {Link, Route, Switch} from 'react-router-dom'
 
 function Home() {
     return <div>
@@ -24,8 +24,16 @@ function Lang(props) {
     return (
         <div>
             {props.match.params.lang}
+            <button onClick={props.history.goBack}>返回</button>
+            <button onClick={() => props.history.push('/home')}>回到首页</button>
         </div>
     )
+}
+
+function NotFound() {
+    return <div>
+        404 Not Found
+    </div>
 }
 
 class RouterTest extends Component {
@@ -37,14 +45,21 @@ class RouterTest extends Component {
                     <li><Link to='/about'>About</Link></li>
                     <li><Link to='/details'>Details</Link></li>
                 </ul>
-                {/*
-                    exact表示精确匹配，如果不配置exact，那么如下所示的/details和/details/:lang，如果路径为/details/java，
+                {/*exact表示精确匹配，如果不配置exact，那么如下所示的/details和/details/:lang，如果路径为/details/java，
                     会同时显示/details 和 /details/:lang的内容，配置exact的话，如果路径为/details/java，那么只会显示
-                    /details/:lang的内容
-                */}
-                <Route exact path='/home' component={Home}/>
-                <Route exact path='/details' component={Details}/>
-                <Route exact path='/details/:lang' component={Lang} />
+                    /details/:lang的内容*/}
+                {/*<Route exact path='/home' component={Home}/>*/}
+                {/*<Route exact path='/details' component={Details}/>*/}
+                {/*<Route exact path='/details/:lang' component={Lang}/>*/}
+
+                {/*如果加上Switch标签，则如果有重复的Route，则只显示第一个匹配到的，同时，如果定义了没有路径的页面，也需要加Switch*/}
+                <Switch>
+                    <Route exact path='/home' component={Home}/>
+                    <Route exact path='/details' component={Details}/>
+                    <Route exact path='/home' component={Home}/>
+                    <Route exact path='/details/:lang' component={Lang}/>
+                    <Route exact component={NotFound}/>
+                </Switch>
 
             </div>
         )
