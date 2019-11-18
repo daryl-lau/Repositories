fc9f206c4b4ca0495e1dea3a29c1c57b0952be0f
jerry <lyucan_1@163.com>
2019-11-18 22:47:38 +0800

    some changes

--- myrepo/nodejs/Koa/表单处理、上传文件、下载文件/server.js
+++ myrepo/nodejs/Koa/表单处理、上传文件、下载文件/server.js
@@ -1,5 +1,6 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const send = require('koa-send');
 
 // 使用此模块进行表单数据获取和文件上传
 const body = require('koa-better-body');
@@ -21,6 +22,13 @@ router.post('/upload', async ctx => {
     console.log(ctx.request.fields);
 
     ctx.body = '上传成功';
+});
+
+
+router.get('/download', async ctx => {
+    const path = `./download/pic.png`;
+    ctx.attachment(path);
+    await send(ctx, path)
 });
 
 server.use(router.routes());
