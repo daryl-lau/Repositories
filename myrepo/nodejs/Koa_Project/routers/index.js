3bf6be9677cc9f17b2ddef38b648bf05de8f3fc1
jerry <lyucan_1@163.com>
2019-11-21 19:51:04 +0800

    some changes

--- myrepo/nodejs/Koa_Project/routers/index.js
+++ myrepo/nodejs/Koa_Project/routers/index.js
@@ -3,7 +3,7 @@ const Router = require('koa-router');
 let router = new Router();
 
 router.get('/', ctx=>{
-    ctx.body='/'
+    ctx.body='index'
 });
 
 router.use('/admin', require('./admin'));
--- myrepo/nodejs/crypto-md5加密/crypto.js
+++ myrepo/nodejs/crypto-md5加密/crypto.js
@@ -2,9 +2,13 @@ const crypto = require('crypto');
 
 const hash = crypto.createHash('md5');
 
-// 多个update，会将各个update的值拼接到一起
+
 hash.update('123');
-hash.update('456');
+
+// 多个update，会将各个update的值拼接到一起
+// hash.update('123');
+// hash.update('456');
+// 想当于hash.update('123456');
 
 let result = hash.digest('hex');
 
